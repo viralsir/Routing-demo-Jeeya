062e6686b26d04c1e6b7031e65fe47631502a878
ViralSir <viralsir2018@gmail.com>
2023-09-07 13:48:43 +0530

    login ,about us and contact us has been added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AboutusComponent} from "./aboutus/aboutus.component";
+import {ContactUsComponent} from "./contact-us/contact-us.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"aboutus",component:AboutusComponent},
+  {path:"contactus",component:ContactUsComponent},
+  {path:"dashboard",component:DashboardComponent},
+  {path:"",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContactUsComponent,
+    AboutusComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
